a70fa7fa8fb22fdabe481685e5ca187cdd21eff0
Trevor Lomba <76894056+trevorlomba@users.noreply.github.com>
2023-04-03 14:01:13 -0400

    visualization and readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Strava-Explora
+
+This project utilizes the Strava API to pull information about runs and analyze them using Python. The data is manipulated and visualized using Pandas, Seaborn, Matplotlib, Numpy, and Datetime libraries.
+Installation
+
+    Create an account on Strava and go to https://www.strava.com/settings/api to obtain your API key.
+
+    Clone or download this repository.
+
+    Install the required libraries using the command: pip install -r requirements.txt.
+
+    Change the following fields in the strava_api.py file to your own Strava API credentials: client_id, client_secret, refresh_token.
+
+    Run the strava_api.py script.
+
+    Run the run_analysis.py script to analyze your Strava runs.
+
+## Usage
+
+The run_analysis.py script retrieves all of your runs for the current year and creates a dataframe with the following columns: name, average_speed, suffer_score, upload_id, type, distance, moving_time, max_speed, total_elevation_gain, start_date_local, average_heartrate, max_heartrate, workout_type, elapsed_time, and average_cadence.
+
+The script then cleans and manipulates the data by converting the date column into a datetime object and breaking it into separate date and time columns, converting distance from meters to miles, calculating pace, and creating additional columns for plotting.
+
+Finally, the script plots the data in multiple graphs.
+
+## Credits
+
+This project was created by Trevor Lomba (tjlomba95@gmail.com) as a personal project. Special thanks to Strava for providing the API and to the creators of the Python libraries used in this project.
--- strava_exploration.py
+++ strava_exploration.py
@@ -76,93 +76,112 @@ print(runs.count(1))
 
 # Plot the data
 sns.set(style="ticks", context="talk")
-plot = sns.regplot(x='distance', y='pace', data=runs).set_title( "Pace vs Distance")
 
-
-fig = plt.figure()  # create overall container
+# Create a first plot
+fig = plt.figure(figsize=(10, 6))  # create overall container
+fig.subplots_adjust(bottom=0.2, left=0.1)  # adjust padding
 ax1 = fig.add_subplot(111)  # add a 1 by 1 plot to the figure
 x = np.asarray(runs.start_date_local)  # convert data to numpy array
 y = np.asarray(runs.pace)
 ax1.plot_date(x, y)  # plot data points in scatter plot on ax1
 ax1.set_title('Average Pace over Time')
-# ax1.set_ylim([0,5])
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Pace (min/mile)')
+
 # add trend line
 x2 = mdates.date2num(x)
 z = np.polyfit(x2, y, 1)
 p = np.poly1d(z)
 plt.plot(x, p(x2), 'r--')
+
 # format the figure and display
 fig.autofmt_xdate(rotation=45)
-# fig.tight_layout()
 
-# Create a third plot
-fig2 = plt.figure()
+# Create a second plot
+fig2 = plt.figure(figsize=(10, 6))
+fig2.subplots_adjust(bottom=0.2, left=0.1)
 ax2 = fig2.add_subplot(111)
 sns.regplot(x='pace', y='cadence', data=runs).set_title("Pace vs Cadence")
+ax2.set_xlabel('Pace (min/mile)')
+ax2.set_ylabel('Cadence (steps/min)')
+
+# Create a third plot
+fig3 = plt.figure(figsize=(10, 6))
+fig3.subplots_adjust(bottom=0.2, left=0.1)
+ax3 = fig3.add_subplot(111)
+sns.regplot(x='average_heartrate', y='cadence', data=runs).set_title(
+    "Average Heart Rate vs Cadence")
+ax3.set_xlabel('Average Heart Rate')
+ax3.set_ylabel('Cadence (steps/min)')
 
 # Create a fourth plot
-fig3 = plt.figure()
-ax3 = fig3.add_subplot(111)
-sns.regplot(x='average_heartrate', y='cadence', data=runs).set_title("Average Heart Rate vs Cadence")
+fig4 = plt.figure(figsize=(10, 6))
+fig4.subplots_adjust(bottom=0.2, left=0.1)
+ax4 = fig4.add_subplot(111)
+sns.regplot(x='distance', y='max_heartrate', data=runs).set_title(
+    "Max Heart Rate vs Distance")
+ax4.set_xlabel('Distance (miles)')
+ax4.set_ylabel('Max Heart Rate')
 
 # Create a fifth plot
-fig4 = plt.figure()
-ax4 = fig4.add_subplot(111)
-sns.regplot(x='distance', y='max_heartrate', data=runs).set_title("Max Heart Rate vs Distance")
-
-# Create a sixth plot
-fig5 = plt.figure()
+fig5 = plt.figure(figsize=(10, 6))
+fig5.subplots_adjust(bottom=0.2, left=0.1)
 ax5 = fig5.add_subplot(111)
-sns.regplot(x='distance', y='suffer_score', data=runs).set_title("Suffer Score vs Distance")
-
+sns.regplot(x='distance', y='suffer_score', data=runs).set_title(
+    "Suffer Score vs Distance")
+ax5.set_xlabel('Distance (miles)')
+ax5.set_ylabel('Suffer Score')
 
 # Create a sixth plot
-fig6 = plt.figure()
+fig6 = plt.figure(figsize=(10, 6))
+fig6.subplots_adjust(bottom=0.2, left=0.1)
 ax6 = fig6.add_subplot(111)
-sns.regplot(y='max_heartrate', x='suffer_score', data=runs).set_title("Max Heart Rate vs Suffer Score")
+sns.regplot(y='max_heartrate', x='suffer_score', data=runs).set_title(
+    "Max Heart Rate vs Suffer Score")
+ax6.set_xlabel('Suffer Score')
+ax6.set_ylabel('Max Heart Rate')
 
-
-# # Create a sixth plot
-# fig7 = plt.figure()
-# ax7 = fig7.add_subplot(111)
-# x = np.asarray(runs.start_date_local)  # convert data to numpy array
-# y = np.asarray(runs.pace)
-# ax7.plot_date(x, y)  # plot data points in scatter plot on ax7
-# ax7.set_title('Average Cadence over Time')
-# # add trend line
-# x2 = mdates.date2num(x)
-# z = np.polyfit(x2, y, 1)
-# p = np.poly1d(z)
-# plt.plot(x, p(x2), 'r--')
-# # format the figure and display
-# fig7.autofmt_xdate(rotation=45)
-
-
-
-
-# Create a eighth plot
-fig8 = plt.figure()
-ax8 = fig8.add_subplot(111)
-
-fig8.autofmt_xdate(rotation=45)
-
-# Filter runs at or over 3 miles
-# runs_over_3_miles = runs[(runs['distance'] >= 3) & (runs['cadence'] >= 165)]
+# Create a seventh plot
+fig7 = plt.figure(figsize=(10, 6))
+fig7.subplots_adjust(bottom=0.2, left=0.1)
+ax7 = fig7.add_subplot(111)
 march_runs = runs[runs['start_date_local'].dt.month == 3]
-
-# Plot with filtered data
-# sns.regplot(x="start_time_hr_int", y="max_heartrate", data=runs_over_3_miles).set_title(
-#     "Max Heart Rate vs Start Time (3+ Miles)")
 sns.regplot(x="start_time_hr_int", y="max_heartrate", data=march_runs).set_title(
     "Max Heart Rate vs Start Time (March)")
+ax7.set_xlabel('Start Time (hour of day)')
+ax7.set_ylabel('Max Heart Rate')
 
+# Create the eighth plot
+fig8 = plt.figure()  # create a new figure
+ax8 = fig8.add_subplot(111)  # add a subplot to the figure
+x = np.asarray(runs.start_date_local)  # extract the start date of each run
+y = np.asarray(runs.pace)  # extract the pace of each run
+ax8.plot_date(x, y)  # plot the pace versus the start date
+ax8.set_title('Average Cadence over Time')  # set the title of the plot
+x2 = mdates.date2num(x)  # convert the start date to a numerical format
+z = np.polyfit(x2, y, 1)  # fit a linear trendline to the data
+# create a polynomial function from the trendline coefficients
+p = np.poly1d(z)
+plt.plot(x, p(x2), 'r--')  # plot the trendline in red dashes
+fig8.autofmt_xdate(rotation=45)  # rotate the x-axis labels for readability
+
+# Create the ninth plot
+fig9 = plt.figure()  # create a new figure
+ax9 = fig9.add_subplot(111)  # add a subplot to the figure
+fig9.autofmt_xdate(rotation=45)  # rotate the x-axis labels for readability
+# extract runs that occurred in March
+march_runs = runs[runs['start_date_local'].dt.month == 3]
+sns.regplot(x="start_time_hr_int", y="max_heartrate", data=march_runs).set_title(
+    "Max Heart Rate vs Start Time (March)")  # plot the maximum heart rate versus the start time for March runs
+# print information about the run with the latest start time
 print(runs.loc[runs['start_time_unix'] == runs['start_time_unix'].max()])
+# print information about the run with the latest start time string
 print(runs.loc[runs['start_time_str'] == runs['start_time_str'].max()])
+# print information about the run with the latest start time object
 print(runs.loc[runs['start_time'] == runs['start_time'].max()])
-
+# sort the runs by the start date
 sorted_runs = runs.sort_values(by=['start_date_local'])
-print(sorted_runs)
+plt.show()  # display the plots in the figure
 
-plt.show()
 
 print(plot)
