908eb03b1c48766fcdd4e47346460e20c97e7147
Trevor Lomba <76894056+trevorlomba@users.noreply.github.com>
2023-04-14 21:23:28 -0400

    visualization updates and cors updates

--- api.py
+++ api.py
@@ -8,19 +8,20 @@ CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 @app.route('/api/mileage-report')
 def mileage_report():
-    data = data_processing.get_mileage_report_data()
-    return jsonify(data)
+    data = jsonify(data_processing.get_mileage_report_data())
+    return data
 
 
 @app.route('/api/cadence-report')
 def cadence_report():
-    data = data_processing.get_cadence_report_data()
-    return jsonify(data)
+    data = jsonify(data_processing.get_cadence_report_data())
+    return data
 
 
 @app.route('/images/<path:image_name>', methods=['GET'])
 def get_image(image_name):
     return send_from_directory('visualizations', image_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
